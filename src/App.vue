99a622dca7cf82b7b39966e33733629fd888be8c
pietroline <pietrobarberi@hotmail.it>
2022-02-18 17:24:08 +0100

    Vue CLI First

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyTitle></MyTitle>
+    <MainJumbotron></MainJumbotron>
+  </div>
+</template>
+
+<script>
+
+  import MyTitle from './components/MyTitle.vue'
+  import MainJumbotron from './components/MainJumbotron.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      MyTitle,
+      MainJumbotron,
+    }
+  }
+  
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/MainJumbotron.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="jumbotron">
+        <h2>Jumbotron</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nihil eos maiores quam recusandae molestias consequatur necessitatibus voluptatem nulla animi? Non, reiciendis optio. Et neque recusandae ipsa nisi, laborum eos.</p>
+        
+        <div class="container">
+
+            <div class="img">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png" alt="img logo HTML 5">
+            </div>
+
+            <div class="img">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/1200px-CSS.3.svg.png" alt="img logo CSS 3">
+            </div>
+
+            <div class="img">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/2560px-Bootstrap_logo.svg.png" alt="img logo BOOTSTRAP">
+            </div>
+
+            <div class="img">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/73/Javascript-736400_960_720.png" alt="img logo JAVASCRIPT">
+            </div>
+
+        </div>
+        
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "MainJumbotron",   
+    }
+</script>
+
+<style scoped>
+    .jumbotron{
+        background-color: #eaeaea;
+
+        width: 100%;
+
+        padding-top: 20px;
+    }
+    
+    .container{
+        display: flex;
+        justify-content: center;
+    }
+
+    .img{
+        width: 50px;
+        height: 50px;
+
+        margin: 20px;
+    }
+
+    .img img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+
+    .img img:hover{
+        cursor: pointer;
+    }
+</style>
